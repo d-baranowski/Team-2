08f60943bfb324e95a22662f92c5b7bfac982b31
olivermc1 <o.mcpheely@gmail.com>
2015-04-14 17:28:22 +0100

    Login with facebook button implemented

--- Lloyds_Bank_Team2_App2/app/src/main/java/com/team/two/lloyds_app/screens/fragments/OptionsFragment.java
+++ Lloyds_Bank_Team2_App2/app/src/main/java/com/team/two/lloyds_app/screens/fragments/OptionsFragment.java
@@ -7,6 +7,7 @@ Date : 06/04/2015
 Purpose : Options
  */
 
+        import android.content.Intent;
         import android.content.pm.ActivityInfo;
         import android.graphics.BitmapFactory;
         import android.os.Bundle;
@@ -19,6 +20,13 @@ Purpose : Options
         import android.widget.Switch;
         import android.widget.TextView;
 
+        import com.facebook.AccessToken;
+        import com.facebook.CallbackManager;
+        import com.facebook.FacebookCallback;
+        import com.facebook.FacebookException;
+        import com.facebook.Profile;
+        import com.facebook.login.LoginResult;
+        import com.facebook.login.widget.LoginButton;
         import com.team.two.lloyds_app.R;
         import com.facebook.FacebookSdk;
 
@@ -35,6 +43,31 @@ public class OptionsFragment extends android.support.v4.app.Fragment {
     private TextView link_facebook_text;
     private Spinner FontSpinner;
 
+    //facebook SDK References
+    private TextView fbWelcomeMessage;
+    private CallbackManager mCallbackManager;
+    private FacebookCallback<LoginResult> mCallBack = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            AccessToken accessToken = loginResult.getAccessToken();
+            Profile profile = Profile.getCurrentProfile();
+            if (profile != null){
+                fbWelcomeMessage.setText("Welcome " + profile.getName());
+            }
+
+        }
+
+        @Override
+        public void onCancel() {
+
+        }
+
+        @Override
+        public void onError(FacebookException e) {
+
+        }
+
+    };
 
     private String[] Font_size = {"1", "2", "3"};
     public OptionsFragment() {
@@ -42,11 +75,17 @@ public class OptionsFragment extends android.support.v4.app.Fragment {
     }
 
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+        mCallbackManager=CallbackManager.Factory.create();
+        }
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+
         Root = inflater.inflate(R.layout.fragment_options, container, false);
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         Spinner FontSpinner = (Spinner) Root.findViewById(R.id.font_size_spinner);
@@ -55,6 +94,24 @@ public class OptionsFragment extends android.support.v4.app.Fragment {
         FontSpinner.setAdapter(adapter);
         return Root;
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        LoginButton loginButton = (LoginButton) getView().findViewById(R.id.login_button);
+        loginButton.setReadPermissions("user_friends");
+        loginButton.setFragment(this);
+        loginButton.registerCallback(mCallbackManager, mCallBack);
+    }
+
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 
     public void onNotificationsSwitchClicked(View view) {
         boolean on = ((Switch) view.findViewById(R.id.notifications_switch)).isChecked();
