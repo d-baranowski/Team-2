b9990e7f59306e5867b290d2220c59126dc3ab58
Mikey Edwards <mikey.edwards95@gmail.com>
2015-04-15 14:02:44 +0100

    Added branch finder search bar and changed popups
    Also tried to fix exceptions

--- Lloyds_Bank_Team2_App2/app/src/main/java/com/team/two/lloyds_app/screens/fragments/BranchFinderFragment.java
+++ Lloyds_Bank_Team2_App2/app/src/main/java/com/team/two/lloyds_app/screens/fragments/BranchFinderFragment.java
@@ -14,6 +14,7 @@ import java.net.ConnectException;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.location.Location;
+import android.location.Geocoder;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -22,9 +23,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.view.View.OnClickListener;
 
 import com.google.android.gms.maps.*;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -46,6 +50,14 @@ public class BranchFinderFragment extends Fragment {
     private GoogleMap googleMap;
     private HashMap<String, Branch> branchMap;
 
+    //Create objects for the popup details
+    RelativeLayout popup;
+    TextView branchTitle;
+    TextView addressTitle;
+    TextView addressView;
+    TextView timesTitle;
+    TextView timesView;
+
     public BranchFinderFragment() {
         // Required empty public constructor
     }
@@ -55,17 +67,31 @@ public class BranchFinderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.fragment_branch_finder, container, false);
+
+        //Create objects for the popup details
+        popup = (RelativeLayout) root.findViewById(R.id.branch_popup);
+        branchTitle = (TextView) root.findViewById(R.id.branch_title);
+        addressTitle = (TextView) root.findViewById(R.id.branch_address_title);
+        addressView = (TextView) root.findViewById(R.id.branch_address);
+        timesTitle = (TextView) root.findViewById(R.id.branch_opening_times_title);
+        timesView = (TextView) root.findViewById(R.id.branch_opening_times);
+
         getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        setSearchButtonListener();
 
         initialiseBranches();
 
-        //If a network error has been thrown,
         try {
             createMapView();
-        } catch (ConnectException ce) {
+        }
+        //If a network error has been thrown, notify user
+        catch (ConnectException ce) {
             Toast.makeText(getActivity(), ce.getMessage(), Toast.LENGTH_LONG).show();
             return root;
-        } catch (NullPointerException npe){
+        }
+        //If the map failed to initiate, notify user
+        catch (NullPointerException npe) {
             Toast.makeText(getActivity(), npe.getMessage(), Toast.LENGTH_LONG).show();
         }
 
@@ -74,12 +100,55 @@ public class BranchFinderFragment extends Fragment {
         return root;
     }
 
+    //Method to respond to search button press
+    private void setSearchButtonListener() {
+
+        Button button = (Button) root.findViewById(R.id.branch_search_button);
+
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                String input = ((EditText) root.findViewById(R.id.branch_search_bar_input)).getText().toString();
+
+                if (!input.isEmpty() || input.equals(null)) {
+
+                    try{
+                        popup.setVisibility(View.INVISIBLE);
+                        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder()
+                            .target(getSearchPosition(input))
+                            .zoom(9)
+                            .build()));
+                    }
+
+                    catch(Exception e){
+                        if(e.getClass().equals(ConnectException.class)){
+                            Toast.makeText(getActivity(), getString(R.string.branch_network_search_error), Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getActivity(), getString(R.string.branch_general_search_error), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+
+                } else {
+                    Toast.makeText(getActivity(), getString(R.string.branch_empty_search_error), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
     //Read branch information from the text file to create Branch objects
     private void initialiseBranches() {
+
         branchMap = new HashMap<>();
+
         try {
+
             Scanner scanner = new Scanner(getResources().getAssets().open("branches.txt"));
+
             while (scanner.hasNextLine()) {
+
                 Branch branch = new Branch(
                         scanner.nextLine(),
                         Double.parseDouble(scanner.nextLine()),
@@ -87,8 +156,11 @@ public class BranchFinderFragment extends Fragment {
                         new Address(scanner.nextLine(), scanner.nextLine(), scanner.nextLine(), scanner.nextLine()),
                         scanner.nextLine(),
                         new String[]{scanner.nextLine(), scanner.nextLine(), scanner.nextLine(), scanner.nextLine(), scanner.nextLine(), scanner.nextLine(), scanner.nextLine()});
+
                 branchMap.put(branch.getName(), branch);
+
             }
+
         } catch (IOException ioe) {
             Log.e("branchFinder", ioe.toString());
         }
@@ -98,12 +170,12 @@ public class BranchFinderFragment extends Fragment {
     private void createMapView() throws ConnectException {
 
         if (!isNetworkAvailable()) {
-            throw new ConnectException("Connection problem: please check your internet settings");
+            throw new ConnectException(getString(R.string.branch_network_search_error));
         }
         googleMap = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.mapView)).getMap();
 
         if (googleMap == null) {
-            throw new NullPointerException("There was a problem loading the map");
+            throw new NullPointerException(getString(R.string.branch_general_map_error));
         }
 
         //Enable the My Location layer
@@ -112,7 +184,7 @@ public class BranchFinderFragment extends Fragment {
         //Get the latest location
         Location lastLocation = ((MainActivity) getActivity()).mLastLocation;
         if (lastLocation == null) {
-            Toast.makeText(getActivity(), "Error getting location: check your GPS settings", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getActivity(), getString(R.string.branch_gps_search_error), Toast.LENGTH_SHORT).show();
         }
 
         //Set up initial zoom to user's current location
@@ -120,14 +192,6 @@ public class BranchFinderFragment extends Fragment {
                 .target(new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude()))
                 .zoom(10)
                 .build()));
-
-        //Create objects for the popup details
-        final RelativeLayout popup = (RelativeLayout) root.findViewById(R.id.branch_popup);
-        final TextView branchTitle = (TextView) root.findViewById(R.id.branch_title);
-        final TextView addressTitle = (TextView) root.findViewById(R.id.branch_address_title);
-        final TextView addressView = (TextView) root.findViewById(R.id.branch_address);
-        final TextView timesTitle = (TextView) root.findViewById(R.id.branch_opening_times_title);
-        final TextView timesView = (TextView) root.findViewById(R.id.branch_opening_times);
 
         //Set up the marker listener to manage marker clicks
         googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
@@ -146,7 +210,6 @@ public class BranchFinderFragment extends Fragment {
                 timesView.setText("");
                 addressView.setText("");
 
-
                 //Populate the popup with branch details
                 branchTitle.setText(branch.getName());
 
@@ -163,7 +226,7 @@ public class BranchFinderFragment extends Fragment {
 
                 //Display the popup
                 popup.setVisibility(View.VISIBLE);
-
+                
                 return false;
             }
         });
@@ -172,6 +235,7 @@ public class BranchFinderFragment extends Fragment {
         googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng ll) {
+                googleMap.mark
                 //Hide the popup
                 popup.setVisibility(View.INVISIBLE);
             }
@@ -207,6 +271,20 @@ public class BranchFinderFragment extends Fragment {
                 = (ConnectivityManager) (getActivity().getSystemService(Context.CONNECTIVITY_SERVICE));
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
+    }
+
+    private LatLng getSearchPosition(String input) throws IOException {
+
+        Geocoder gc = new Geocoder(getActivity());
+
+        if(!isNetworkAvailable()){
+            throw new ConnectException();
+        }
+
+        android.location.Address address = gc.getFromLocationName(input, 1).get(0);
+
+        return new LatLng(address.getLatitude(), address.getLongitude());
+
     }
 
 
